756bcee8a2e16062e7078bda3d742d5db04c4444
Developer Experience team at Tendermint <hello@tendermint.com>
2020-09-19 21:34:18 +0700

    Initialized with Starport

---
+++ x/clay/client/cli/tx.go
@@ -0,0 +1,29 @@
+package cli
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/faddat/clay/x/clay/types"
+)
+
+// GetTxCmd returns the transaction commands for this module
+func GetTxCmd(cdc *codec.Codec) *cobra.Command {
+	clayTxCmd := &cobra.Command{
+		Use:                        types.ModuleName,
+		Short:                      fmt.Sprintf("%s transactions subcommands", types.ModuleName),
+		DisableFlagParsing:         true,
+		SuggestionsMinimumDistance: 2,
+		RunE:                       client.ValidateCmd,
+	}
+
+	clayTxCmd.AddCommand(flags.PostCommands(
+    // this line is used by starport scaffolding
+	)...)
+
+	return clayTxCmd
+}
