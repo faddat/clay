2aee7d49e3c26169d45ba83f37bc1c69aea23780
Jacob Gadikian <jacobgadikian@gmail.com>
2020-09-22 19:44:41 +0700

    added wasm and post title body

--- x/clay/client/rest/rest.go
+++ x/clay/client/rest/rest.go
@@ -9,4 +9,6 @@ import (
 // RegisterRoutes registers clay-related REST handlers to a router
 func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router) {
   // this line is used by starport scaffolding
+	r.HandleFunc("/clay/post", listPostHandler(cliCtx, "clay")).Methods("GET")
+	r.HandleFunc("/clay/post", createPostHandler(cliCtx)).Methods("POST")
 }
