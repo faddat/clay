2be4143fda30a863a81cfda528fa359cc7ce543c
Jacob Gadikian <jacobgadikian@gmail.com>
2020-09-22 16:50:12 +0700

    clayy number 18

--- vue/src/store/app.js
+++ vue/src/store/app.js
@@ -1,5 +1,6 @@
 module.exports = {
   types: [
     // this line is used by starport scaffolding
+		{ type: "post", fields: ["title", "body", ] },
   ],
 };
--- x/clay/keeper/querier.go
+++ x/clay/keeper/querier.go
@@ -2,6 +2,8 @@ package keeper
 
 import (
   // this line is used by starport scaffolding
+	"github.com/faddat/clay/x/clay/types"
+		
 	abci "github.com/tendermint/tendermint/abci/types"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -13,6 +15,8 @@ func NewQuerier(k Keeper) sdk.Querier {
 	return func(ctx sdk.Context, path []string, req abci.RequestQuery) ([]byte, error) {
 		switch path[0] {
     // this line is used by starport scaffolding # 2
+		case types.QueryListPost:
+			return listPost(ctx, k)
 		default:
 			return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, "unknown clay query endpoint")
 		}
