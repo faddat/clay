101da2bec9a2a2fa5b8639ae23c77be65b9e8284
Jacob Gadikian <jacobgadikian@gmail.com>
2020-10-15 13:04:26 +0700

    add blog fix standup

--- x/clay/client/rest/rest.go
+++ x/clay/client/rest/rest.go
@@ -9,4 +9,11 @@ import (
 // RegisterRoutes registers clay-related REST handlers to a router
 func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router) {
   // this line is used by starport scaffolding # 1
+		r.HandleFunc("/clay/post", createPostHandler(cliCtx)).Methods("POST")
+		r.HandleFunc("/clay/post", listPostHandler(cliCtx, "clay")).Methods("GET")
+		r.HandleFunc("/clay/post/{key}", getPostHandler(cliCtx, "clay")).Methods("GET")
+		r.HandleFunc("/clay/post", setPostHandler(cliCtx)).Methods("PUT")
+		r.HandleFunc("/clay/post", deletePostHandler(cliCtx)).Methods("DELETE")
+
+		
 }
