851e1cfb9f70942c35c47313ac79e6449676f09c
Jacob Gadikian <jacobgadikian@gmail.com>
2020-10-19 18:16:08 +0700

    Added types

--- x/clay/client/cli/tx.go
+++ x/clay/client/cli/tx.go
@@ -23,6 +23,9 @@ func GetTxCmd(cdc *codec.Codec) *cobra.Command {
 
 	clayTxCmd.AddCommand(flags.PostCommands(
     // this line is used by starport scaffolding # 1
+		GetCmdCreatePost(cdc),
+		GetCmdSetPost(cdc),
+		GetCmdDeletePost(cdc),
 	)...)
 
 	return clayTxCmd
